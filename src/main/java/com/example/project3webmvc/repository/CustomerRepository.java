0eef9836bbca731cbe88a96fd5b64bc6892c8bd7
Bell <85625700+Bell1609@users.noreply.github.com>
2023-05-21 21:54:45 +0700

    Add files via upload

--- src/main/java/com/example/project3webmvc/repository/CustomerRepository.java
+++ src/main/java/com/example/project3webmvc/repository/CustomerRepository.java
@@ -4,4 +4,5 @@ import com.example.project3webmvc.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+    Customer findByCustomerIDAndPassword(int customerID, String password);
 }
